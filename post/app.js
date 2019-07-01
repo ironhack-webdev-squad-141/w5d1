91dec99e417dc704ec562ed76d8bc1c7a8746354
mjarraya <mjarraya@student.42.fr>
2019-07-01 12:06:43 +0200

    middleware setup

--- post/app.js
+++ post/app.js
@@ -5,13 +5,34 @@ app.set("view engine", "hbs");
 
 app.use(express.urlencoded({ extended: true }));
 
+const greetOnRequest = greeting => {
+  return (req, res, next) => {
+    req.secretKey = "123";
+    console.log(greeting);
+    next();
+  };
+};
+
+app.use(greetOnRequest("Hello there is a new request"));
+
+app.use(greetOnRequest("Can you see this?"));
+
+const toLowerCase = () => {
+  return (req, res, next) => {
+    //code here
+  };
+};
+
+app.use(toLowerCase());
+
 app.get("/", (req, res) => {
+  console.log("secret key: ", req.secretKey);
   res.render("index");
 });
 
 app.post("/users", (req, res) => {
   console.log(req.body);
-  res.send(req.body);
+  //   res.send(req.body);
 });
 
 app.listen(3000);
