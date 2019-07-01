34220e3b3191aa29721250cd80f0e8d39060a7fb
mjarraya <mjarraya@student.42.fr>
2019-07-01 14:14:43 +0200

    seeding db

--- library-project/models/Book.js
+++ library-project/models/Book.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const bookSchema = new Schema({});
+const bookSchema = new Schema(
+  {
+    title: String,
+    author: String,
+    rating: Number,
+    description: String
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
 
 const Book = mongoose.model("Book", bookSchema);
 
