a27034b64c131ad83edfbf5c81c0b2b9e547a8f0
mjarraya <mjarraya@student.42.fr>
2019-07-01 16:48:53 +0200

    update

--- library-project/routes/index.js
+++ library-project/routes/index.js
@@ -33,6 +33,33 @@ router.post("/books/add", (req, res) => {
     });
 });
 
+router.get("/books/edit/:bookId", (req, res) => {
+  Book.findById(req.params.bookId)
+    .then(book => {
+      res.render("book-edit", { book });
+    })
+    .catch(err => {
+      console.log("Error getting the book: ", err);
+    });
+});
+
+router.post("/books/edit/:bookId", (req, res) => {
+  const { title, author, description, rating } = req.body;
+
+  Book.findByIdAndUpdate(req.params.bookId, {
+    title,
+    author,
+    description,
+    rating
+  })
+    .then(() => {
+      res.redirect(`/books/${req.params.bookId}`);
+    })
+    .catch(err => {
+      console.log("Error while updating the book: ", err);
+    });
+});
+
 router.get("/books/:bookId", (req, res) => {
   // get the information for the book whose id is bookId from the database
   Book.findById(req.params.bookId)
